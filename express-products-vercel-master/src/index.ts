619f4e6ff0553a7c3fcb955c8c2e446576ed1492
evgeniy <yevgeniy5@ukr.net>
2022-12-10 22:53:39 +0200

    first commit

---
+++ express-products-vercel-master/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express'
+import { productsRouter } from './routers/products-router';
+import { runDb } from './repositories/db';
+
+const app = express()
+const port = process.env.PORT || 5000
+
+const parserMiddleware = express.json()
+app.use(parserMiddleware)
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Samurai')
+})
+
+app.use('/products', productsRouter)
+
+const startApp = async () => {
+  await runDb()
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+startApp()
